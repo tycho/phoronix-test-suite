c3f53af19aa463f0785d4831706037abb97e0541
Steven Noonan <steven@uplinklabs.net>
2026-02-03 12:02:06 -0800

    juliagpu-1.3.1: patch OpenCL platform selection
    It fails if there's more than one platform because it doesn't know how
    to select between platforms based on devices available.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- ob-cache/test-profiles/local/juliagpu-1.3.1/install.sh
+++ ob-cache/test-profiles/local/juliagpu-1.3.1/install.sh
@@ -3,6 +3,57 @@
 tar -xvjf JuliaGPU-v1.2pts-1.tar.bz2
 cd JuliaGPU-v1.2pts/
 
+patch -Np1 <<"EOF"
+diff --git a/juliaGPU b/juliaGPU
+index f404b5d..ea85b51 100755
+Binary files a/juliaGPU and b/juliaGPU differ
+diff --git a/juliaGPU.c b/juliaGPU.c
+index cac9ea7..2eda5fd 100644
+--- a/juliaGPU.c
++++ b/juliaGPU.c
+@@ -143,6 +143,30 @@ static char *ReadSources(const char *fileName) {
+ 
+ }
+ 
++static cl_platform_id pick_platform_for_type(cl_device_type type) {
++    cl_uint numPlatforms = 0;
++    cl_int st = clGetPlatformIDs(0, NULL, &numPlatforms);
++    if (st != CL_SUCCESS || numPlatforms == 0) return NULL;
++
++    cl_platform_id *plats = malloc(sizeof(*plats) * numPlatforms);
++    st = clGetPlatformIDs(numPlatforms, plats, NULL);
++    if (st != CL_SUCCESS) { free(plats); return NULL; }
++
++    cl_platform_id chosen = NULL;
++
++    for (cl_uint i = 0; i < numPlatforms; i++) {
++        cl_uint n = 0;
++        st = clGetDeviceIDs(plats[i], type, 0, NULL, &n);
++        if (st == CL_DEVICE_NOT_FOUND || n == 0) continue;   // this platform doesn't expose that type
++        if (st != CL_SUCCESS) continue;                      // some other issue, skip or handle
++        chosen = plats[i];
++        break;
++    }
++
++    free(plats);
++    return chosen;
++}
++
+ static void SetUpOpenCL() {
+ 	cl_device_type dType;
+ 	if (useCPU) {
+@@ -191,8 +215,8 @@ static void SetUpOpenCL() {
+ 			fprintf(stderr, "OpenCL Platform %d: %s\n", i, pbuf);
+ 		}
+ 
+-		platform = platforms[0];
+ 		free(platforms);
++		platform = pick_platform_for_type(dType);
+ 	}
+ 
+ 	cl_context_properties cps[3] ={
+EOF
+
 case $OS_TYPE in
 	"MacOSX")
 		CCFLAGS="-O3 -march=native -ftree-vectorize -funroll-loops -Wall -framework OpenCL -framework OpenGL -framework GLUT"
