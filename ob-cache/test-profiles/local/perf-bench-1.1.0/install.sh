c9d2c6c0b7cf2133937697835125c1a67f9b1387
Steven Noonan <steven@edera.dev>
2026-02-20 10:29:35 -0800

    perf-bench-1.1.0: properly remove kernel source tree after build
    Signed-off-by: Steven Noonan <steven@edera.dev>

--- ob-cache/test-profiles/local/perf-bench-1.1.0/install.sh
+++ ob-cache/test-profiles/local/perf-bench-1.1.0/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-tar -xf linux-6.9.tar.xz
-cd linux-6.9/tools/perf
+KVER=6.9
+tar -xf linux-${KVER}.tar.xz
+pushd "linux-${KVER}/tools/perf"
 NO_LIBTRACEEVENT=1 make -j $NUM_CPU_CORES
 echo $? > ~/install-exit-status
-cp perf ~
-cd ~
-rm -rf linux-6.2.6
+cp perf ../../../
+popd
+rm -rf linux-${KVER}
 echo "#!/bin/sh
 ./perf bench \$@ > \$LOG_FILE 2>&1
 echo \$? > ~/test-exit-status" > perf-bench
